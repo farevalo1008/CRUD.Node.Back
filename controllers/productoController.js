bf4298706ffb1a37232fe85a7b18bfac8a514db2
farevalo1008 <fer.f1arevalo@gmail.com>
2021-05-23 19:12:46 -0400

    termine el curso ANGULAR NODE

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,3 +1,91 @@
-exports.crearProducto = (req, res) => {
-    console.log('desde crear producto')
+const Producto = require("../config/models/Producto");
+
+exports.crearProducto = async (req, res) => {
+    
+    try {
+        let producto;
+
+        //Creamos nuestro producto
+        producto = new Producto(req.body);
+
+        await producto.save();
+        res.send(producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.obtenerProductos = async (req, res) => {
+    
+    try {
+
+        const producto = await Producto.find();
+        res.json(producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.actualizarProducto = async (req, res) => {
+    
+    try {
+        const{ nombre, categoria, ubicacion, precios } = req.body;
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            res.status(404).json({ msg: 'No existe el producto'})
+        }
+
+        producto.nombre = nombre;
+        producto.categoria = categoria;
+        producto.ubicacion = ubicacion;
+        producto.precios = precios;
+
+        producto = await Producto.findOneAndUpdate({ _id: req.params.id }, producto, { new:true } )
+        res.json(producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.obtenerProducto = async (req, res) => {
+    
+    try {
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            res.status(404).json({ msg: 'No existe el producto'})
+        }
+
+        res.json(producto);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.eliminarProducto = async (req, res) => {
+    
+    try {
+    let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            res.status(404).json({ msg: 'No existe el producto'}) 
+        }
+
+        await Producto.findOneAndRemove({ _id: req.params.id })
+        
+        res.json({msg: 'Producto eliminado con exito'});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require("cors")
 
 // Creamos el servidor
 const app = express();
@@ -7,14 +8,11 @@ const app = express();
 // BASE DE DATOS USER: fernando PASSWORD: Fa257153462019
 //Conectar a la DB
 conectarDB();
+app.use(cors());
+
+app.use(express.json());
 
 app.use('/api/productos', require('./routes/producto'));
-
-//definimos Ruta principal
-app.get('/', (req, res) => {
-    res.send('Hola mundo');
-
-})
 
 app.listen(4000, () => {
     console.log('El servidor esta corriendo correctamente');
